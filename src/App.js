2a3e613012819ccf5f02a80a0341f2525c4e4883
Andre Luis <andre.luismesquita@hotmail.com>
2022-08-08 14:31:29 -0300

    feat: footer created and styled

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Topics from "./components/Topics";
 import GlobalStyle from "./styles/global";
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Header />
       <Topics />
+      <Footer />
     </>
   );
 }
